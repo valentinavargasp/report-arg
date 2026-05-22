87a57c056b974af53eebdc1c0327015c05047ae5
Juanemiliani70 <juanmaemiliani_97@hotmail.com>
2026-05-21 23:27:40 -0300

    SCRUM-89: Crear categoria - frontend

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,6 @@
-import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export async function middleware(request) {
-  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-
-  if (!token) {
-    const loginUrl = new URL("/login", request.url);
-    loginUrl.searchParams.set("callbackUrl", request.nextUrl.pathname);
-    return NextResponse.redirect(loginUrl);
-  }
-
+export function middleware(request) {
   return NextResponse.next();
 }
 
